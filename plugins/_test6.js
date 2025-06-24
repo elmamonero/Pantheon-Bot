7a971c932687e1eb24b0f78e41fca5208551aa30
elmamonero <debastado0@gmail.com>
2025-06-24 17:29:28 -0400

    Update _test6.js

--- plugins/_test6.js
+++ plugins/_test6.js
@@ -96,7 +96,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['play <texto>'];
 handler.tags = ['descargas'];
-handler.command = ['play'];
+handler.command = ['play6'];
 
 export default handler;
 
