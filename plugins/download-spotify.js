1ebbfb081497603c7e39b580e1ffedb823578201
elmamonero <debastado0@gmail.com>
2026-01-14 15:02:15 -0400

    Update download-spotify.js

--- plugins/download-spotify.js
+++ plugins/download-spotify.js
@@ -7,9 +7,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (m.fromMe) return;
 
   if (!text) {
-    const usage = `╭────═[ PANTHEON BOT ]═─────⋆
+    const usage = `╭──═[ PANTHEON BOT ]═───⋆
 │ 🎵 *SPOTIFY DOWNLOADER*
 │ Uso: ${usedPrefix + command} <nombre o enlace>
+
 │ Ej:
 │ • ${usedPrefix + command} I Can't Stop Me
 │ • ${usedPrefix + command} https://open.spotify.com/track/37ZtpRBkHcaq6hHy0X98zn
